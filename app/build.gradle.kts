1f98f2f27cc170cdf6a937ab12cde90feb6bd75b
fredo <fredo@T480-Thinkpad>
2025-09-19 22:32:20 +0200

    build.gradle sync

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "vet.derichs.compendium"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
