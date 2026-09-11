9e614abbfb85c4e1cebc21c4cfc3ab7925ea42ae
fredo <info@derichs.be>
2026-09-11 21:48:53 +0200

    version bump to 0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "vet.derichs.compendium"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.10"
+        versionCode = 11
+        versionName = "0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
